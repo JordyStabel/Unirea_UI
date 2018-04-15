742ee462e223912d59608e34d52500543d913c54
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-04-15 19:08:08 +0200

    Initial commit

---
+++ Assets/Scripts/Timed_UI_Screen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Unirea.UI
+{
+    public class Timed_UI_Screen : UI_Screen
+    {
+        #region Varibles & Properties
+
+        [Header("Properties")]
+        public float waitTime = 2f;
+        public UnityEvent onWaitCompleted = new UnityEvent();
+
+        private float startTime;
+
+        #endregion
+
+        #region Methods
+
+        public override void StartScreen()
+        {
+            base.StartScreen();
+
+            startTime = Time.time;
+            
+        }
+
+        public override void CloseScreen()
+        {
+            base.CloseScreen();
+            StartCoroutine(WaitForTime());
+        }
+
+        IEnumerator WaitForTime()
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            if (onWaitCompleted != null)
+            {
+                onWaitCompleted.Invoke();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/UI_Screen.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Unirea.UI
+{
+    // Requirements for a screen (¡Muy Importante!) Without the animations won't work
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(CanvasGroup))]
+    public class UI_Screen : MonoBehaviour
+    {
+        #region Varibles & Properties
+
+        [Header("Properties")]
+        public Selectable startSelectable;
+
+        [Header("Events")]
+        public UnityEvent onScreenStart = new UnityEvent();
+        public UnityEvent onScreenClose = new UnityEvent();
+
+        private Animator animator;
+
+        #endregion
+
+
+        #region Methods
+
+        // Use this for initialization
+        void Awake()
+        {
+            animator = GetComponent<Animator>();
+
+            // If there is a startSelectable --> highlight the startSelectable gameobject
+            if (startSelectable)
+            {
+                EventSystem.current.SetSelectedGameObject(startSelectable.gameObject);
+            }
+        }
+
+        public virtual void StartScreen()
+        {
+            // If there are listeners --> start event
+            if (onScreenStart != null)
+            {
+                onScreenStart.Invoke();
+            }
+
+            HandleAnimator("show");
+        }
+
+        public virtual void CloseScreen()
+        {
+            // If there are listeners --> start event
+            if (onScreenClose != null)
+            {
+                onScreenClose.Invoke();
+            }
+
+            HandleAnimator("hide");
+        }
+
+        private void HandleAnimator(string triggerType)
+        {
+            if (animator)
+            {
+                animator.SetTrigger(triggerType);
+            }
+        }
+
+        #endregion
+    }
+}
