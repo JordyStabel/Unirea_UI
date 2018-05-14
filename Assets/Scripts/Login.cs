99caf4ceb61ab65af3de43e416ee6672bda093a0
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-05-14 10:14:15 +0200

    added login script, building UI toggle

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour {
+
+    public InputField userName;
+    public InputField passWord;
+	
+    public void UserLogin()
+    {
+        Debug.Log(userName.text + " / " + passWord.text);
+        // User login code here
+    }
+}
