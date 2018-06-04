9a5fa841b9fb377a90f28a71f244a2e1401d110c
Martens,Stan S.W.T <stan.martens@student.fontys.nl>
2018-06-04 15:15:54 +0200

    fix player

--- Assets/Backend/Models/Player.cs
+++ Assets/Backend/Models/Player.cs
@@ -5,25 +5,26 @@ namespace Assets.Backend.Models
     public class Player
     {
         public string AuthenticationToken { get; set; }
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
-        public Player(int id, string authenticationToken, string username, string email, string password)
+        public Player(string authenticationToken, string username, string email, string password)
         {
+            Id = 0;
             AuthenticationToken = authenticationToken;
             Username = username;
             Email = email;
             Password = password;
         }
 
-        public Player(int id, string authenticationToken, string username, string password)
+        public Player(int id, string authenticationToken, string username, string email)
         {
+            Id = id;
             AuthenticationToken = authenticationToken;
             Username = username;
-            Email = null;
-            Password = password;
+            Email = email;
         }
     }
 }
