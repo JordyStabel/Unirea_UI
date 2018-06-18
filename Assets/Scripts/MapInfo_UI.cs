f10efbd7b89910c19a4f1f6871b2d8b7ca580c93
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-06-18 11:54:47 +0200

    towns now load and update each time screen gets loaded

--- Assets/Scripts/MapInfo_UI.cs
+++ Assets/Scripts/MapInfo_UI.cs
@@ -1,24 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class MapInfo_UI : MonoBehaviour {
-
-    public GameObject UI;
-    public Text townName;
-
-    private Tile tile;
-
-	public void Show(Tile _tile)
+namespace Unirea.UI
+{
+    public class MapInfo_UI : MonoBehaviour
     {
-        tile = _tile;
 
-        townName.text = _tile.name + "\n" + "Henk";
+        public GameObject UI;
+        public Text townName;
 
-        UI.SetActive(true);
-    }
-	
-    public void Hide()
-    {
-        UI.SetActive(false);
+        private Tile tile;
+
+        public void Show(Tile _tile)
+        {
+            tile = _tile;
+
+            townName.text = _tile.name + "\n" + "Henk";
+
+            UI.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            UI.SetActive(false);
+        }
     }
 }
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -6,108 +6,120 @@ using UnityEngine.UI;
 using Assets.Backend.Rest;
 using Assets.Backend.RestModels;
 
-public class MapManager : MonoBehaviour {
-
-    public static MapManager instance;
-    public MapInfo_UI mapInfo_UI;
-
-    public GameObject tile;
-    public Sprite[] sprites;
-    public Sprite townSprite;
-    public GridLayoutGroup content;
-    public float snappingSpeed;
-
-    public int maxWidth;
-    public int maxHeight;
-
-    private GameObject[,] tileArray;
-
-    private List<Vector2> townList = new List<Vector2>();
-    private List<RestTown> allTowns = new List<RestTown>();
-
-    private MapRest mapRest = new MapRest();
-
-    void Awake()
+namespace Unirea.UI
+{
+    public class MapManager : MonoBehaviour
     {
-        if (instance != null)
+
+        public static MapManager instance;
+        public Dropdown townSelector;
+        public MapInfo_UI mapInfo_UI;
+
+        public GameObject tile;
+        public Sprite[] sprites;
+        public Sprite townSprite;
+        public GridLayoutGroup content;
+        public float snappingSpeed;
+
+        public int maxWidth;
+        public int maxHeight;
+
+        private GameObject[,] tileArray;
+
+        private List<Vector2> townList = new List<Vector2>();
+        private List<TownRest> allTowns = new List<TownRest>();
+
+        private MapRest mapRest = new MapRest();
+
+        void Awake()
         {
-            Debug.Log("More than one build managers in the scene");
-        }
-        instance = this;
-    }
-
-    void Start()
-    {
-        tileArray = new GameObject[maxWidth, maxHeight];
-
-        townList.Add(new Vector2(1, 4));
-        townList.Add(new Vector2(3, 14));
-        townList.Add(new Vector2(7, 7));
-
-        GenerateMap();
-    }
-
-    private void GenerateMap()
-    {
-        for (int x = 0; x < maxHeight; x++)
-        {
-            for (int y = 0; y < maxWidth; y++)
+            if (instance != null)
             {
-                GameObject _tile = Instantiate(tile, content.transform);
-                _tile.name = x + "," + y;
-                _tile.GetComponentInChildren<Text>().text = x + "," + y;
-                Tile tileScript = _tile.GetComponent<Tile>();
-                tileScript.coordinates = new Vector2(y, x);
-                tileArray[x, y] = _tile;
-                int arrayIndex = UnityEngine.Random.Range(0, sprites.Length);
-                Sprite sprite = sprites[arrayIndex];
-                _tile.GetComponent<Image>().sprite = sprite;
+                Debug.Log("More than one build managers in the scene");
+            }
+            instance = this;
+        }
+
+        void Start()
+        {
+            tileArray = new GameObject[maxWidth, maxHeight];
+
+            townList.Add(new Vector2(1, 4));
+            townList.Add(new Vector2(3, 14));
+            townList.Add(new Vector2(7, 7));
+
+            GenerateMap();
+        }
+
+        void OnEnable()
+        {
+            EventManager.mapUpdateEvent += UpdateMap;
+        }
+
+        private void OnDisable()
+        {
+            EventManager.mapUpdateEvent -= UpdateMap;
+        }
+
+        private void GenerateMap()
+        {
+            for (int x = 0; x < maxHeight; x++)
+            {
+                for (int y = 0; y < maxWidth; y++)
+                {
+                    GameObject _tile = Instantiate(tile, content.transform);
+                    _tile.name = x + "," + y;
+                    _tile.GetComponentInChildren<Text>().text = x + "," + y;
+                    Tile tileScript = _tile.GetComponent<Tile>();
+                    tileScript.coordinates = new Vector2(y, x);
+                    tileArray[x, y] = _tile;
+                    int arrayIndex = UnityEngine.Random.Range(0, sprites.Length);
+                    Sprite sprite = sprites[arrayIndex];
+                    _tile.GetComponent<Image>().sprite = sprite;
+                }
             }
         }
 
-        foreach (Vector2 coordinate in townList)
+        public void SelectTown(Tile tile)
         {
-            tileArray[Convert.ToInt32(coordinate.x), Convert.ToInt32(coordinate.y)].GetComponent<Image>().sprite = townSprite;
-            Tile test = tileArray[Convert.ToInt32(coordinate.x), Convert.ToInt32(coordinate.y)].GetComponent<Tile>();
-            test.name = "Henk " + coordinate;
+            if (tile.name.Contains("Henk"))
+                mapInfo_UI.Show(tile);
+            else
+                mapInfo_UI.Hide();
+
+            StopAllCoroutines();
+            StartCoroutine(SnapToTown(tile.coordinates));
         }
-    }
 
-    public void SelectTown(Tile tile)
-    {
-        if (tile.name.Contains("Henk"))
-            mapInfo_UI.Show(tile);
-        else
-            mapInfo_UI.Hide();
-
-        StopAllCoroutines();
-        StartCoroutine(SnapToTown(tile.coordinates));
-    }
-
-    IEnumerator SnapToTown(Vector2 coordinates)
-    {
-        Vector3 targetPosition = new Vector3(((coordinates.x * 250) - 415) * -1, (coordinates.y * 250) -835);
-
-        while (Vector3.Distance(content.transform.localPosition, targetPosition) > 50f)
+        IEnumerator SnapToTown(Vector2 coordinates)
         {
-            content.transform.localPosition = Vector3.Lerp(
-                content.GetComponent<RectTransform>().localPosition, 
-                new Vector3(((coordinates.x * 250) - 415) * -1, (coordinates.y * 250) - 835), 
-                Time.deltaTime * snappingSpeed);
-            yield return null;
+            Vector3 targetPosition = new Vector3(((coordinates.x * 250) - 415) * -1, (coordinates.y * 250) - 835);
+
+            while (Vector3.Distance(content.transform.localPosition, targetPosition) > 50f)
+            {
+                content.transform.localPosition = Vector3.Lerp(
+                    content.GetComponent<RectTransform>().localPosition,
+                    new Vector3(((coordinates.x * 250) - 415) * -1, (coordinates.y * 250) - 835),
+                    Time.deltaTime * snappingSpeed);
+                yield return null;
+            }
         }
-    }
 
-    public async void GetTowns()
-    {
-        string token = PlayerInfo.currentPlayer.AuthenticationToken;
-        Debug.Log(token);
-
-        allTowns = await mapRest.GetAllTowns();
-
-        foreach (RestTown town in allTowns)
+        public async void UpdateMap()
         {
-            Debug.Log(town.Id);
+            string token = PlayerInfo.currentPlayer.AuthenticationToken;
+
+            List<Map> map = await mapRest.GetAllTowns();
+
+            foreach (Map town in map)
+                CreateTown(town);
+        }
+
+        public void CreateTown(Map town)
+        {
+            tileArray[Convert.ToInt32(town.x), Convert.ToInt32(town.y)].GetComponent<Image>().sprite = townSprite;
+            Tile newTown = tileArray[Convert.ToInt32(town.x), Convert.ToInt32(town.y)].GetComponent<Tile>();
+            newTown.name = "Henk " + town.id;
         }
     }
 }
