ac5993f92ff6bf7b0fce5a9034d3a85e4da08bd9
stan martens <stan-martens@hotmail.com>
2018-06-23 23:56:28 +0200

    Update playertown model

--- Assets/Backend/RestModels/PlayerTownWrapper.cs
+++ Assets/Backend/RestModels/PlayerTownWrapper.cs
@@ -9,10 +9,23 @@ namespace Assets.Backend.RestModels
 {
     public class TownResources
     {
+        public int wood;
+        public int oil;
+        public int iron;
     }
 
     public class TownBuildings
     {
+        public int oilRefinery { get; set; }
+        public int ironMine { get; set; }
+        public int forestry { get; set; }
+        public int headquarters { get; set; }
+        public int warehouse { get; set; }
+        public int barracks { get; set; }
+        public int trainingGrounds { get; set; }
+        public int foundry { get; set; }
+        public int ammunitionDepot { get; set; }
+        public int wall { get; set; }
     }
 
     public class PlayerTown
