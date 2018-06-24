19cc4dc1db974b93d147ad068cd24cf5c4f63c40
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-06-24 17:35:49 +0200

    wip Creating troops

---
+++ Assets/Scripts/BuildingScripts/Barracks.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Backend.Rest;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Backend;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Barracks : MonoBehaviour {
+
+    public Slider infantrySlider;
+    public Slider cavalrySlider;
+    public Slider armoredSlider;
+
+    private ArmyRest armyRest = new ArmyRest();
+
+	public void TrainTroops()
+    {
+        CreateTroops();
+    }
+
+    private async CreateTroops()
+    {
+        bool responds = await armyRest.TrainArmy(PlayerInfo.currentPlayer.AuthenticationToken, ArmyType.Infantry, infantrySlider.value, PlayerInfo.currrentTown.townId);
+    }
+}
