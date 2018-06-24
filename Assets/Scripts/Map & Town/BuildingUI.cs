96cf841ada99b6ee504de51e14ab4ce3c267d3fa
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-06-24 15:10:14 +0200

    building now also show level

--- Assets/Scripts/Map & Town/BuildingUI.cs
+++ Assets/Scripts/Map & Town/BuildingUI.cs
@@ -36,12 +36,18 @@ namespace Unirea.UI
             building = PlayerInfo.currrentTown.townBuildings[(int)buildingType - 1];
             try
             {
+                if (building.level > 1)
+                {
+                    buildingCanvas.GetComponent<Image>().sprite = allBuildingSprites[1];
+                }
                 buildingCanvas.GetComponent<Image>().sprite = allBuildingSprites[building.level];
+                buildingCanvas.GetComponentInChildren<Text>().text = building.name + " " + building.level;
             }
             catch
             {
                 // No image
                 buildingCanvas.GetComponent<Image>().sprite = allBuildingSprites[0];
+                buildingCanvas.GetComponentInChildren<Text>().text = building.name + " " + building.level;
                 Debug.Log("Sprites aren't made yet.");
             }
         }
