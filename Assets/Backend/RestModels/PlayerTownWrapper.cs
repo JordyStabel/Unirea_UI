5e8d1a7ce5253cf2eea843fe38871324a7538d5e
stan martens <stan-martens@hotmail.com>
2018-06-24 11:38:21 +0200

    Update playertown model

--- Assets/Backend/RestModels/PlayerTownWrapper.cs
+++ Assets/Backend/RestModels/PlayerTownWrapper.cs
@@ -1,41 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Assets.Backend.Models.Buildings;
+﻿using System.Collections.Generic;
 
 namespace Assets.Backend.RestModels
 {
-    public class TownResources
+    public class TownResource
     {
-        public int wood;
-        public int oil;
-        public int iron;
+        public string name { get; set; }
+        public int amount { get; set; }
     }
 
-    public class TownBuildings
+    public class UpgradeCost
     {
-        public int oilRefinery { get; set; }
-        public int ironMine { get; set; }
-        public int forestry { get; set; }
-        public int headquarters { get; set; }
-        public int warehouse { get; set; }
-        public int barracks { get; set; }
-        public int trainingGrounds { get; set; }
-        public int foundry { get; set; }
-        public int ammunitionDepot { get; set; }
-        public int wall { get; set; }
+        public string name { get; set; }
+        public int amount { get; set; }
+    }
+
+    public class TownBuilding
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int level { get; set; }
+        public List<UpgradeCost> upgradeCost { get; set; }
+        public int resourceProduction { get; set; }
     }
 
     public class PlayerTown
     {
-        public TownResources townResources { get; set; }
-        public TownBuildings townBuildings { get; set; }
+        public List<TownResource> townResources { get; set; }
+        public List<TownBuilding> townBuildings { get; set; }
         public int x { get; set; }
         public int y { get; set; }
         public string username { get; set; }
         public string name { get; set; }
         public int townId { get; set; }
+        public int playerId { get; set; }
     }
 }
