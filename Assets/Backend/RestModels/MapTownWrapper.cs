3f1202a0a8d0f1b25d358900b41592f51c2b3b56
stan martens <stan-martens@hotmail.com>
2018-06-24 21:44:38 +0200

    Add playerId to map towns

--- Assets/Backend/RestModels/MapTownWrapper.cs
+++ Assets/Backend/RestModels/MapTownWrapper.cs
@@ -12,6 +12,7 @@ namespace Assets.Backend.RestModels
         public int id { get; set; }
         public int x { get; set; }
         public int y { get; set; }
+        public int playerId { get; set; }
     }
 
     public class RootObject
